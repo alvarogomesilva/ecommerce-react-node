572268436e1ba8b9694c5f547ba824ffd36bd9b0
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-05-13 20:04:45 -0300

    feat: adiciona userId ao Request

---
+++ backend/src/controllers/users/profile-user-controller.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+
+export async function profile(request: Request, response: Response) {
+    
+    response.status(200).send()
+}
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { create } from "../controllers/users/create-user-controller";
 import { autenticate } from "../controllers/users/auth-user-controller";
+import { profile } from "../controllers/users/profile-user-controller";
 
 
 const route = Router()
 
 route.post('/users', create)
 route.post('/session', autenticate)
+
+route.get('/me', profile)
 
 export { route as usersRoutes }
