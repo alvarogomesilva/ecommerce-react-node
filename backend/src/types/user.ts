b1fbddad9ce85b917b7d7982004c5c9bd8964e7d
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-05-13 16:16:09 -0300

    feat: cria repository user

---
+++ backend/src/types/user.ts
@@ -0,0 +1,14 @@
+export type CreateUserDto = {
+    name: string
+    email: string
+    password: string
+}
+
+export type User = {
+    id: string
+    name: string
+    email: string
+    role: string
+    createdAt: Date
+    updatedAt: Date
+}
