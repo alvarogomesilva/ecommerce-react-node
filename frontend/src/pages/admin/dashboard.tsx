535488ae0500688991158e980bad312a3922af2b
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-05-18 14:00:18 -0300

    feat: adiciona cards admin

--- frontend/src/pages/admin/dashboard.tsx
+++ frontend/src/pages/admin/dashboard.tsx
@@ -2,7 +2,36 @@
 export function Dashboard() {
   return (
     <div className="container">
-      <h2>Dashboard</h2>
+      <div className="row justify-content-between mt-4">
+        <div className="card col-6 col-md-3 mx-auto" style={{ width: '18rem' }}>
+          <div className="card-body">
+            <h5 className="card-title">Categorias</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+
+          </div>
+        </div>
+        <div className="card col-6 col-md-3 mx-auto" style={{ width: '18rem' }}>
+          <div className="card-body">
+            <h5 className="card-title">Sub Categorias</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+
+          </div>
+        </div>
+        <div className="card col-6 col-md-3 mx-auto" style={{ width: '18rem' }}>
+          <div className="card-body">
+            <h5 className="card-title">Produtos</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+
+          </div>
+        </div>
+        <div className="card col-6 col-md-3 mx-auto" style={{ width: '18rem' }}>
+          <div className="card-body">
+            <h5 className="card-title">Banners</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
