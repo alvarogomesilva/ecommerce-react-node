a24d0b4dd242fb95f9c8895887bb1e9b5090e32c
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-05-24 13:14:58 -0300

    feat: adiciona modal de cadastro de categoria

--- frontend/src/pages/admin/admin-categories.tsx
+++ frontend/src/pages/admin/admin-categories.tsx
@@ -3,8 +3,36 @@ export function AdminCategories() {
 
     return (
         <main className="container">
+            <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Nova categoria</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            <form>
+                                <div className="mb-3">
+                                    <label htmlFor="recipient-name" className="col-form-label">Nome</label>
+                                    <input type="text" className="form-control" id="recipient-name" />
+                                </div>
+                            </form>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+                            <button type="button" className="btn btn-primary">Cadastrar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="d-flex justify-content-end">
-                <button type="button" className="btn btn-primary m-3">Nova categoria</button>
+                <button
+                    type="button"
+                    className="btn btn-primary m-3"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal">
+                    Nova categoria
+                </button>
             </div>
 
             <table className="table table-sm table-responsive align-middle">
