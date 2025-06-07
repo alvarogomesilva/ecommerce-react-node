c806b9c2aff49cb8999706cc712a14a7aebe56b8
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-06-07 13:18:13 -0300

    feat: adiciona tabela de caracteristicas dos produtos

---
+++ backend/prisma/migrations/20250607161428_create_table_characteristic/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "characteristics" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "characteristics_pkey" PRIMARY KEY ("id")
+);
