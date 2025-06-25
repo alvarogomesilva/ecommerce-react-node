6bf5cfd0df5ef8160fe73ef92eaf05b7e38971f4
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-06-25 20:16:46 -0300

    refactor: tipando o retorno das caracteristicas

--- frontend/src/api/products-characteristics/list-all-products-characteristics.ts
+++ frontend/src/api/products-characteristics/list-all-products-characteristics.ts
@@ -1,7 +1,19 @@
 import { api } from "../../lib/axios";
 
 
-export async function listAllCharacteristics(productId: string) {
-    const response = await api.get(`/product-characteristics/${productId}`)
-    return response.data
+export interface Characteristics {
+  id: string
+  description: string
+  addition: number
+  hex_value: string
+}
+
+
+export type CharacteristicsResponse = {
+  [key: string]: Characteristics[];
+}
+
+export async function listAllCharacteristics(productId: string): Promise<CharacteristicsResponse> {
+  const response = await api.get<CharacteristicsResponse>(`/product-characteristics/${productId}`)
+  return response.data
 }
